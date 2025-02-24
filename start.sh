e7422bf265595436f7d254fd05ed03a8cc994e0e
Vaishnav Sandeep <90813931+itsmehecker@users.noreply.github.com>
2025-02-25 00:11:11 +0530

    add start.sh

---
+++ start.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/yourusername/raspapi.git
+cd raspapi
+python -m venv .venv
+source .venv/bin/activate
+pip install wheel
+pip install --upgrade setuptools pip
+pip install -r requirements.txt
+touch .env
+nano .env
+uvicorn main:app --reload
